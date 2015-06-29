93a439187febd8cc0f5dda30b7e954c3f3e26824
Matt Carter <m@ttcarter.com>
2015-06-29 13:23:22 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Reflib-CLI
+==========
+Command line interface for [Reflib](https://github.com/hash-bang/Reflib-Node).
+
+
+	Usage: app [file...]
+	
+	Options:
+	
+	  -h, --help     output usage information
+	  -V, --version  output the version number
+	  -c, --count    Dont output refs, just output the count
+	  -v, --verbose  Be verbose
---
+++ app.js
@@ -0,0 +1,60 @@
+#!/usr/bin/node
+var async = require('async-chainable');
+var colors = require('colors');
+var fs = require('fs');
+var program = require('commander');
+var reflib = require('reflib');
+var util = require('util');
+
+program
+	.version(require('./package.json').version)
+	.usage('[file...]')
+	.option('-c, --count', 'Dont output refs, just output the count')
+	.option('-v, --verbose', 'Be verbose')
+	.parse(process.argv);
+
+async()
+	.set('refs', [])
+	.set('refsCount', 0)
+	.forEach(program.args, function(next, file) {
+		fs.exists(file, function(exists) {
+			if (!exists) return next('File not found: ' + file);
+			next();
+		});
+	})
+	.forEach(program.args, function(next, file) {
+		var self = this;
+		if (program.verbose) console.log(colors.grey('Processing file', file));
+		var thisCount = 0;
+		reflib.parseFile(file)
+			.on('error', function(err) {
+				return next(err);
+			})
+			.on('ref', function(ref) {
+				thisCount++;
+				if (program.count) {
+					self.refsCount++;
+				} else {
+					self.refs.push(ref);
+				}
+			})
+			.on('end', function() {
+				if (program.verbose) console.log(colors.grey('Finished parsing', file, 'with', thisCount,'references'));
+				next();
+			});
+	})
+	.then(function(next) {
+		if (program.count) {
+			console.log('Found', colors.cyan(this.refsCount), 'references');
+		} else {
+			console.log(util.inspect(this.refs, {depth: null, colors: true}));
+		}
+		next();
+	})
+	.end(function(err) {
+		if (err) {
+			console.log(colors.red('ERROR'), err);
+			process.exit(1);
+		}
+		process.exit(0);
+	});
